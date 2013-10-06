9fbe0b54717056161b244de955b21c90df0da142
Juan Pablo Garibotti Arias <jp.garibotti@gmail.com>
2013-10-06 02:02:32 -0300

    - Fixed project so it runs with static glew library.

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -75,47 +75,47 @@ lib/liblibgltools-d.a: ${OBJECTFILES}
 ${OBJECTDIR}/_ext/196920952/MVPMatrix.o: /C/Users/JoPe/Documents/NetBeansProjects/libgltools/src/MVPMatrix.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/196920952
 	${RM} $@.d
-	$(COMPILE.cc) -g -Wall -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/196920952/MVPMatrix.o /C/Users/JoPe/Documents/NetBeansProjects/libgltools/src/MVPMatrix.cpp
+	$(COMPILE.cc) -g -Wall -DGLEW_STATIC -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/196920952/MVPMatrix.o /C/Users/JoPe/Documents/NetBeansProjects/libgltools/src/MVPMatrix.cpp
 
 ${OBJECTDIR}/src/Frame.o: src/Frame.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -Wall -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Frame.o src/Frame.cpp
+	$(COMPILE.cc) -g -Wall -DGLEW_STATIC -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Frame.o src/Frame.cpp
 
 ${OBJECTDIR}/src/GLMatrix.o: src/GLMatrix.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -Wall -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/GLMatrix.o src/GLMatrix.cpp
+	$(COMPILE.cc) -g -Wall -DGLEW_STATIC -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/GLMatrix.o src/GLMatrix.cpp
 
 ${OBJECTDIR}/src/GeometryTransform.o: src/GeometryTransform.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -Wall -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/GeometryTransform.o src/GeometryTransform.cpp
+	$(COMPILE.cc) -g -Wall -DGLEW_STATIC -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/GeometryTransform.o src/GeometryTransform.cpp
 
 ${OBJECTDIR}/src/MatrixStack.o: src/MatrixStack.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -Wall -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/MatrixStack.o src/MatrixStack.cpp
+	$(COMPILE.cc) -g -Wall -DGLEW_STATIC -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/MatrixStack.o src/MatrixStack.cpp
 
 ${OBJECTDIR}/src/Model.o: src/Model.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -Wall -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Model.o src/Model.cpp
+	$(COMPILE.cc) -g -Wall -DGLEW_STATIC -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Model.o src/Model.cpp
 
 ${OBJECTDIR}/src/Shader.o: src/Shader.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -Wall -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Shader.o src/Shader.cpp
+	$(COMPILE.cc) -g -Wall -DGLEW_STATIC -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Shader.o src/Shader.cpp
 
 ${OBJECTDIR}/src/Triangle.o: src/Triangle.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -Wall -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Triangle.o src/Triangle.cpp
+	$(COMPILE.cc) -g -Wall -DGLEW_STATIC -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Triangle.o src/Triangle.cpp
 
 ${OBJECTDIR}/src/Vertex.o: src/Vertex.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
 	${RM} $@.d
-	$(COMPILE.cc) -g -Wall -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Vertex.o src/Vertex.cpp
+	$(COMPILE.cc) -g -Wall -DGLEW_STATIC -Iinclude -I../libmatrix/include -I../libvector/include -I/C/lib/glew-1.10.0/include -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/Vertex.o src/Vertex.cpp
 
 # Subprojects
 .build-subprojects:
